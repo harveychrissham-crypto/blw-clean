c640646f40c7dda41db9cd1c27f26c32b28336c6
harveychrissham-crypto <harveychrissham@gmail.com>
2026-08-13 11:01:13 +0300

    Protect sermon management writes

--- server/routes/sermonRoutes.js
+++ server/routes/sermonRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { listSermons, createSermon, updateSermon, deleteSermon, setFeaturedSermon } from '../controllers/sermonController.js';
+import { requireLeaderAdmin } from '../middleware/leaderAdminMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', listSermons);
-router.post('/', createSermon);
-router.put('/:id', updateSermon);
-router.put('/:id/feature', setFeaturedSermon);
-router.delete('/:id', deleteSermon);
+router.post('/', requireLeaderAdmin, createSermon);
+router.put('/:id', requireLeaderAdmin, updateSermon);
+router.put('/:id/feature', requireLeaderAdmin, setFeaturedSermon);
+router.delete('/:id', requireLeaderAdmin, deleteSermon);
 
 export default router;
