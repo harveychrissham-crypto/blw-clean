432b18a3f2d21bd2472f13e260d5c43c491dc0fc
harveychrissham-crypto <harveychrissham@gmail.com>
2026-08-13 11:01:18 +0300

    Protect venue management writes

--- server/routes/venueRoutes.js
+++ server/routes/venueRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { listVenues, getVenueByChapter, upsertVenue, deleteVenue } from '../controllers/venueController.js';
+import { requireLeaderAdmin } from '../middleware/leaderAdminMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', listVenues);
 router.get('/:chapter', getVenueByChapter);
-router.put('/:chapter', upsertVenue);
-router.delete('/:chapter', deleteVenue);
+router.put('/:chapter', requireLeaderAdmin, upsertVenue);
+router.delete('/:chapter', requireLeaderAdmin, deleteVenue);
 
 export default router;
