4296231639326fc5705b6aedf5d4441ad5099c58
harveychrissham-crypto <harveychrissham@gmail.com>
2026-08-13 11:01:29 +0300

    Protect member registry endpoints

--- server/routes/memberRoutes.js
+++ server/routes/memberRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { listMembers, searchMembers, checkInMember } from '../controllers/memberController.js';
+import { requireLeaderAdmin } from '../middleware/leaderAdminMiddleware.js';
 
 const router = express.Router();
 
-router.get('/', listMembers);
-router.get('/search', searchMembers);
-router.post('/:membershipId/checkin', checkInMember);
+router.get('/', requireLeaderAdmin, listMembers);
+router.get('/search', requireLeaderAdmin, searchMembers);
+router.post('/:membershipId/checkin', requireLeaderAdmin, checkInMember);
 
 export default router;
