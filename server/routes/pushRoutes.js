4af76b46f6b04d2ed1e3f1862d55698ac394c6ba
harveychrissham-crypto <harveychrissham@gmail.com>
2026-08-15 18:22:00 +0300

    add protected push send route

--- server/routes/pushRoutes.js
+++ server/routes/pushRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { authenticateToken } from '../middleware/authMiddleware.js';
-import { registerToken, unregisterToken } from '../controllers/pushController.js';
+import { requireLeaderAdmin } from '../middleware/leaderAdminMiddleware.js';
+import { registerToken, unregisterToken, sendNotification } from '../controllers/pushController.js';
 
 const router = express.Router();
 
 router.post('/register', authenticateToken, registerToken);
 router.post('/unregister', authenticateToken, unregisterToken);
+router.post('/send', requireLeaderAdmin, sendNotification);
 
 export default router;
