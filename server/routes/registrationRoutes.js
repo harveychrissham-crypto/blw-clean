45232d39a2d582d2d5f4dd4997ef2a91c616af5b
harveychrissham-crypto <harveychrissham@gmail.com>
2026-08-22 19:32:27 +0300

    security: remove unused registration route

--- server/routes/registrationRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { submitRegistration } from '../controllers/registrationController.js';
-
-const router = express.Router();
-router.post('/', submitRegistration);
-
-export default router;
