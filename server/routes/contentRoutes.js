d85e36f48fad3318161e43188fb0f3ae47b37035
harveychrissham-crypto <harveychrissham@gmail.com>
2026-08-22 19:32:36 +0300

    security: remove dead unauthenticated content route

--- server/routes/contentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { listContent, createContent } from '../controllers/contentController.js';
-
-const router = express.Router();
-
-router.get('/', listContent);
-router.post('/', createContent);
-
-export default router;
