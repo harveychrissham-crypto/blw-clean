e2e0eba914c31857d9715a409cc3a8c653b04bfe
harveychrissham-crypto <harveychrissham@gmail.com>
2026-09-09 09:57:04 +0300

    fix: import Capacitor PluginMethod for meeting PiP

--- client/android/app/src/main/java/com/blwcampusministry/app/MeetingPiP.java
+++ client/android/app/src/main/java/com/blwcampusministry/app/MeetingPiP.java
@@ -8,6 +8,7 @@ import android.util.Rational;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
 @CapacitorPlugin(name = "MeetingPiP")
