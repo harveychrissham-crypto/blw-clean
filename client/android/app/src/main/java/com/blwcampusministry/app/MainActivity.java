9c79a3699e00dd71321e718455ece009c29a7289
harveychrissham-crypto <harveychrissham@gmail.com>
2026-09-09 09:48:30 +0300

    feat: enable Android meeting picture-in-picture

--- client/android/app/src/main/java/com/blwcampusministry/app/MainActivity.java
+++ client/android/app/src/main/java/com/blwcampusministry/app/MainActivity.java
@@ -1,12 +1,30 @@
 package com.blwcampusministry.app;
 
+import android.app.PictureInPictureParams;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Rational;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    private volatile boolean meetingPipEnabled = false;
+
+    public void setMeetingPipEnabled(boolean enabled) {
+        meetingPipEnabled = enabled;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         registerPlugin(ApkInstallerPlugin.class);
+        registerPlugin(MeetingPiP.class);
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onUserLeaveHint() {
+        if (meetingPipEnabled && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+            enterPictureInPictureMode(new PictureInPictureParams.Builder().setAspectRatio(new Rational(16, 9)).build());
+        }
+        super.onUserLeaveHint();
     }
 }
