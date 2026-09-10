b1b35d8f28a1d1900dd3bb1d250e2663ed0acc99
harveychrissham-crypto <harveychrissham@gmail.com>
2026-09-10 13:07:09 +0300

    refactor: move stories out of home feed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,6 @@ import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { FiHeart, FiUsers, FiBookOpen, FiGlobe, FiArrowRight, FiMapPin } from 'react-icons/fi';
 import { Card, Eyebrow } from '../components/ui/Card';
-import StoriesRow from '../components/StoriesRow';
 
 const features = [
   {
@@ -64,8 +63,6 @@ export default function Home() {
           </motion.div>
         </motion.div>
       </div>
-
-      <StoriesRow />
 
       <div className="mx-auto max-w-6xl px-5 py-16">
         <div className="grid gap-4 sm:grid-cols-3">
